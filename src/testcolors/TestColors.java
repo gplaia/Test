7c9224213907d66fc00c7c4eda287ef76b22a3bc
gabriella.plaia <gabriella.plaia@verizon.net>
2019-10-05 18:23:29 -0400

    This is a test

---
+++ src/testcolors/TestColors.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testcolors;
+import javax.swing.JFrame;
+/**
+ *
+ * @author Gab
+ */
+public class TestColors {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame jframe = new JFrame();
+        jframe.setBounds(10,10,1000,600);
+        jframe.setVisible(true);
+    }
+    
+}
